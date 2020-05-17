d3a582a2145b86362507ca18b3de9ae2ffee5962
Alyssa <philliak9348@uwec.edu>
2020-05-16 20:08:49 -0500

    update

--- provider/aac.js
+++
@@ -1,7 +0,0 @@
-testButton.addEventListener('click', () => {
-    fetch("http://localhost:3000/aac")
-        .then(reponse => response.json())
-        .then(data => {
-            document.getElementById('test').value="success";
-        });
-    });
--- provider/app.js
+++ provider/app.js
@@ -9,29 +9,60 @@ let credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
 let connection = mysql.createConnection(credentials);
 connection.connect();
 
-function boxesRowToObject(row) {
+function devRTO(row) {
+    return{
+        id:row.id,
+        name:row.name
+    };
+}
+function rowToObject(row) {
     return {
         id: row.id,
         text:row.text,
         image:row.image
-    }
+    };
 }
 
-app.get('/Boxes/', (request, reponse) => {
-    const query = 'SELECT id, text, image FROM Boxes';
+app.get('/Devices', (request, response) => {
+    const query= 'SELECT * FROM DEVICES';
     connection.query(query, (error, rows) => {
         response.send({
-            ok: true,
-            boxes: row.map(boxesRowToObject)
+            ok:true,
+            Devices: row.map(devRTO),
+        });
+    });
+});
+
+app.get('/Boxes', (request, response) => {
+    const query ='SELECT * FROM Boxes';
+    connection.query(query, (error, rows) => {
+        response.send({
+            ok:true,
+            Boxes: rows.map(rowToObject),
+        });
+    });
+});
+
+app.post('/Boxes', (requset,response) => {
+    const query = 'INSERT INTO Boxes(id, text, image) VALUES (?,?,?)';
+    const params = [request.body.id, request.body.text, request.body.image];
+    connection.query(query, params, (error, result) => {
+        response.send({
+            ok:true,
+            id:result.insertId,
+        });
+    });
+});
+
+app.post('/Devices', (request,response) => {
+    const query = 'INSERT INTO Devices(id, name) VALUES (?,?)';
+    const param = [request.body.id, request.body.name];
+    connection.query(query, params, (error, result) => {
+        response.send({
+            ok:true,
+            id:result, insertId,
         });
     });
 });
 
 const port = 3000;
-app.get('/aac', (request, response) => {
-    //send what?
-    response.sendFile('C:/Users/phill/IdeaProjects/aac-server/provider/aac.html');
-});
-app.listen(port, () => {
-    console.log("We're live!");
-});
